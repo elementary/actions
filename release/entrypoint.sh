4a82ea8a30ab7507a1d5ad51d2d2be270c15171c
kgrubb <madison.grubb@sailr.co>
2019-10-18 16:41:27 -0400

    clean up git-related checkouts and rebases

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 if [ "$1" != "--dry-run" ]; then
+  # make sure branches are up-to-date
+  git fetch
   echo "Setting up git credentials..."
   git remote set-url origin https://x-access-token:"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git
   git config --global user.email "action@github.com"
@@ -9,27 +11,8 @@ if [ "$1" != "--dry-run" ]; then
   echo "Git credentials configured."
 fi
 
-#------------------------#
-# Get Branch Information #
-#------------------------#
-
-# Checkout the branch that was merged into (eg. master).
-if [ -n "$GITHUB_REF" ]; then
-  BRANCH="$(basename"$GITHUB_REF")"
-else
-  BRANCH="master"
-fi
-echo "Checking out $BRANCH..."
-git checkout "$BRANCH"
-# Ensure that the local branch is exactly the same as the remote.
-git reset --hard origin/"$BRANCH"
-echo "$BRANCH checked out."
-
-# TODO: use the translations script to check for translation updates
-# update-translations
-
 # Check if this is a release branch that was merged in
-if ! git log --oneline  master -1 | grep "[release]"; then
+if ! git log --oneline -1 | grep -F "[release]" >/dev/null; then
   # not a release, nothing to do here.
   echo "not a release."
   exit 0
@@ -53,7 +36,7 @@ echo "Version: $VERSION"
 # get the release notes, remove any empty lines & padded spacing
 RELEASE_NOTE_RAW="$(xmlstarlet sel -t -v '//release[1]' -n data/"$APPDATA"| awk 'NF'| awk '{$1=$1}1')"
 echo "Release Note Content:"
-echo "$RELEASE_NOTE_RAW"
+echo -e "$RELEASE_NOTE_RAW\n"
 
 #-----------------------#
 # Create Github Release #
@@ -75,12 +58,11 @@ DATA="
 "
 
 if [ "$1" != "--dry-run" ]; then
-  echo -e "data contents:\n$DATA"
   # push the release content to github!
   if ! curl --data "$DATA" https://api.github.com/repos/"$GITHUB_REPOSITORY"/releases?access_token="$GITHUB_TOKEN"; then
     echo "\033[0;31mERROR: Unable to post github release tag information!\033[0m"  && exit 1
   fi
-  echo "A new github release tag has been created!"
+  echo -e "\n\033[1;32mA new github release tag has been created!\033[0m\n"
 else
   echo -e "curl contents:\ncurl --data $DATA https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN"
 fi
@@ -89,28 +71,38 @@ fi
 # Update Debian Changelog #
 #-------------------------#
 
+# make sure we are up to date on the master branch
+git checkout master
+git reset --hard origin/master
+
 # get all commit subjects since last tag
 COMMITS="$(git log "$(git describe --tags "$(git rev-list --tags --max-count=1)")"..HEAD --pretty="format:%s")"
 # filter out commits involving translations and commits that don't have a related merge number
 FILTERED_COMMITS="$(echo "$COMMITS" | awk '!/Weblate/' | awk '!/weblate/' | grep '(#')"
-# Get the first changelog entry
-FIRST_CHANGE="$(echo "$FILTERED_COMMITS" | head -n 1)"
 
 # move to the debian packaging branch
 if ! git checkout deb-packaging; then
   echo "\033[0;31mERROR: Unable to checkout the 'deb-packaging' branch. Does it exist?\033[0m" && exit 1
 fi
 
-# Create a versioned release and add the first line of the changelog
-dch -v "$VERSION" "$FIRST_CHANGE"
-# iterate over any other changelog entries, if there are any
-REMAINING_CHANGES="$(echo "$FILTERED_COMMITS"| tail -n +2)"
-if [ -n "$REMAINING_CHANGES" ]; then
-  while read -r line; do
-    # Append another list item to the changelog
-    dch -a "$line"
-  done <<< "$REMAINING_CHANGES"
+if [ -z "$FILTERED_COMMITS" ]; then
+  echo "no changes since last tag, an empty debian changelog will be created."
+  dch -v "$VERSION" "no changes since last release."
+else
+  # Get the first changelog entry
+  FIRST_CHANGE="$(echo "$FILTERED_COMMITS" | head -n 1)"
+  # Create a versioned release and add the first line of the changelog
+  dch -v "$VERSION" "$FIRST_CHANGE"
+  # iterate over any other changelog entries, if there are any
+  REMAINING_CHANGES="$(echo "$FILTERED_COMMITS"| tail -n +2)"
+  if [ -n "$REMAINING_CHANGES" ]; then
+    while read -r line; do
+      # Append another list item to the changelog
+      dch -a "$line"
+    done <<< "$REMAINING_CHANGES"
+  fi
 fi
+
 # Set the release channel/distro
 dch -r bionic
 
@@ -126,7 +118,7 @@ else
   if ! git push && git push origin "$TAG"; then
     echo "\033[0;31mERROR: Unable to push changelog information!\033[0m" && exit 1
   fi
-  echo "Changelogs have been pushed to deb-packaging!"
+  echo -e "\n\033[1;32mChangelogs have been pushed to deb-packaging!\033[0m\n"
 fi
 
 #------------------#
@@ -134,18 +126,23 @@ fi
 #------------------#
 
 if [ "$1" != "--dry-run" ]; then
+  # make sure we are up to date on the master branch before rebasing stable
+  git checkout master
+  git reset --hard origin/master
+
   # checkout or create stable branch
   if ! git show-ref --verify --quiet refs/heads/stable; then
     git checkout -b stable
   else
     git checkout stable
   fi
-  # rebase with master & push to remote
-  if ! git rebase master; then
+
+  # rebase off of master & push to remote
+  if ! git rebase origin/master; then
     echo "\033[0;31mERROR: Unable to merge master into stable!\033[0m" && exit 1
   fi
-  if ! git push origin stable; then
+  if ! git push origin stable --force-with-lease; then
     echo "\033[0;31mERROR: Unable to push changes to stable branch!\033[0m" && exit 1
   fi
-  echo "The stable branch has been updated successfully!"
+  echo -e "\n\033[1;32mThe stable branch has been updated successfully!\033[0m\n"
 fi
