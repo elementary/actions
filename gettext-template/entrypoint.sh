fbe2ae681bbf6d1af4a99ae463e7bff60b9bf2f3
Corentin Noël <corentin@elementary.io>
2020-05-26 10:28:27 +0200

    gettext: Use apt-get build-dep . to install the dependencies (#44)

--- gettext-template/entrypoint.sh
+++ gettext-template/entrypoint.sh
@@ -50,9 +50,8 @@ if ! git checkout deb-packaging; then
   echo "\033[0;31mERROR: Unable to checkout the 'deb-packaging' branch. Does it exist?\033[0m" && exit 1
 fi
 
-# Create a fake package depending on the build dependency and install/remove it
 sudo apt-get -qq update
-mk-build-deps --build-dep --install --remove --tool 'apt-get -qq' --root-cmd sudo debian/control
+sudo apt-get --no-install-recommends -qq build-dep .
 
 echo -e "\n\033[1;32mInstalled all the build dependencies!\033[0m\n"
 
